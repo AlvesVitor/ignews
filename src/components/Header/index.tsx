0d7a8b322bb7125217888a2171cf6cf11359df22
@vitoralves <01vitoralves@gmail.com>
2021-12-15 17:19:24 -0300

    primeiro commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,23 @@
+import { SigInButton } from "../SignInButton";
+import styles from "./styles.module.scss";
+
+export function Header() {
+
+    return (
+        <header className={styles.headerContainer}>
+
+            <div className={styles.headerContent}>
+                <img src="/images/logo.svg" alt="ig.news" />
+
+                <nav className={styles.active}>
+                    <a>Home</a>
+                    <a>Posts</a>
+                </nav>
+
+                <SigInButton />
+            </div>
+
+        </header>
+
+    )
+}
