89c0af887c616210aa62e4d1a928960982910fa2
alvesvitor <01vitoralves@gmail.com>
2022-02-08 22:53:01 -0300

    att stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,38 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
 import { stripe } from "../services/stripe";
 
+
+//client-side
+//server-side
+//static site generation
+
 import styles from "./home.module.scss";
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
 
   return (
     <>
       <Head>
         <title>Home | ig.news</title>
       </Head>
-
       <main className={styles.contentContainer}>
+
         <section className={styles.hero}>
-          <span>Hey, welcome</span>
+          <span>👏 Hey, welcome</span>
           <h1>News about the <span>React</span> world.</h1>
-          <p>Get access to all publications <br />
-            <span>for $9.90 month</span>
+          <p>Get access to all the publications <br />
+            <span>for  {product.amount} month</span>
           </p>
-
-          <SubscribeButton />
-
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
@@ -31,16 +41,21 @@ export default function Home() {
 }
 
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  const price = await stripe.prices.retrieve("price_1K716dEXX5YRu5s5TlTLMgVC", {
-    expand: ['product']
-  })
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve("price_1K716dEXX5YRu5s5TlTLMgVC");
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100),
+  }
+
 
 
   return {
-    props: {
-
-    }
+    props: { product }, revalidate: 60 * 60 * 24, // 24 horas
   }
 
 }
